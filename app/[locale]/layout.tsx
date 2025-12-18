e48411f5824a059608c034316afaa16878f5d19f
TEFUDA <88384254+TEFUDA@users.noreply.github.com>
2025-12-18 17:35:28 +0100

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
+import DevButtons from '@/components/DevButtons'
 import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,6 +26,7 @@ export default async function LocaleLayout({
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
           {children}
+          <DevButtons />
         </NextIntlClientProvider>
       </body>
     </html>
